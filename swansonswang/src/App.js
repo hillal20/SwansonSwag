064fd819604e8c6be99e0f6d25284761b6566145
hilal aissani <hilalaissani@gmail.com>
2019-03-31 19:33:03 -0400

    wisdom

--- swansonswang/src/App.js
+++ swansonswang/src/App.js
@@ -9,10 +9,12 @@ class App extends Component {
     super(props);
     this.state = {
       wisdom: "",
-      pictureClicked: false
+      allQuotes: [],
+      pictureClicked: false,
+      wisdomSize: "any"
     };
   }
-  componentWillMount() {
+  componentDidMount() {
     this.fetchingWisdom();
   }
   getWisdom = () => {
@@ -20,21 +22,91 @@ class App extends Component {
     this.fetchingWisdom();
   };
   fetchingWisdom = async () => {
-    const promise = await axios.get(
-      "https://ron-swanson-quotes.herokuapp.com/v2/quotes"
-    );
-    this.setState({ wisdom: promise.data[0] });
-  };
+    const promise = await Promise.all([
+      axios.get("https://ron-swanson-quotes.herokuapp.com/v2/quotes"),
+      axios.get("https://ron-swanson-quotes.herokuapp.com/v2/quotes/100")
+    ]);
 
+    console.log("==> promise", promise);
+    this.setState({
+      wisdom: promise[0].data[0],
+      allQuotes: promise[1].data
+    });
+  };
+  wisdomSize = e => {
+    switch (e.target.value) {
+      case "Small":
+        this.setState({ wisdomSize: "small" });
+        break;
+      case "Large":
+        this.setState({ wisdomSize: "large" });
+        break;
+      case "Medium":
+        this.setState({ wisdomSize: "medium" });
+        break;
+      case "Any":
+        this.setState({ wisdomSize: "any" });
+      default:
+        break;
+    }
+  };
+  allQuotes = size => {
+    let splitWords;
+
+    switch (size) {
+      case "small":
+        splitWords = this.state.allQuotes.filter(e => {
+          return e.split(" ").length <= 4;
+        });
+        break;
+      case "medium":
+        splitWords = this.state.allQuotes.filter(e => {
+          return e.split(" ").length >= 5 && e.split(" ").length <= 12;
+        });
+        break;
+      case "large":
+        splitWords = this.state.allQuotes.filter(e => {
+          return e.split(" ").length >= 13;
+        });
+        break;
+      default:
+        break;
+    }
+    return splitWords.map(e => {
+      return <div className="wisdom">{e}</div>;
+    });
+  };
   render() {
-    console.log(this.state.wisdom);
+    if (
+      this.state.allQuotes !== undefined &&
+      this.state.allQuotes[0] !== undefined
+    ) {
+      console.log("k===>", this.state.allQuotes[0].split(" "));
+    }
+
     return (
       <div className="App">
-        <h1>+++++ SWonSWang Wisdom ++++++</h1>
-        <img src={SwansonSwag} onClick={this.getWisdom} width="400" />
-        {this.state.pictureClicked && (
+        <h1>+++++ Swanson Wisdom ++++++</h1>
+        <h3>Select the size of the wisdom: </h3>
+        <select onChange={this.wisdomSize}>
+          <option>Any</option>
+          <option>Small</option>
+          <option>Medium</option>
+          <option>Large</option>
+        </select>
+        <img src={SwansonSwag} onClick={this.getWisdom} width="300" />
+        {this.state.pictureClicked && this.state.wisdomSize === "any" && (
           <div className="wisdom">{this.state.wisdom}</div>
         )}
+        {this.state.pictureClicked &&
+          this.state.wisdomSize === "small" &&
+          this.allQuotes("small")}
+        {this.state.pictureClicked &&
+          this.state.wisdomSize === "large" &&
+          this.allQuotes("large")}
+        {this.state.pictureClicked &&
+          this.state.wisdomSize === "medium" &&
+          this.allQuotes("medium")}
       </div>
     );
   }
